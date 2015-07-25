e1a13201881fb44fbd5de79faa4aa3f27b99081b
Matteo Battaglio <m4dbat@gmail.com>
2015-07-25 03:52:50 +0200

    First draft of a closed-form expression evaluator in the playground.

--- Examples.playground/Contents.swift
+++ Examples.playground/Contents.swift
@@ -33,3 +33,66 @@ let v5b: Vector = [2, 2, 2, 2, 2]
 (v5a + v5b).coordinates
 
 v5b.squareLength
+
+//struct Domain: CollectionType {
+//    
+//}
+
+struct Expression {
+    
+    typealias Operation = (value: Double, function: (Double, Double) -> Double)
+    
+    var operations: [Operation] = []
+    
+    init() {
+        
+    }
+    
+    init(_ operations: [Operation]) {
+        self.operations = operations
+    }
+    
+    func evaluateFor(value: Double) -> Double {
+        return operations.reduce(value) { (acc: Double, operation: Operation) -> Double in
+            operation.function(acc, operation.value)
+        }
+    }
+    
+//    func evaluate<Interval: IntervalType>(#interval: Interval, step: Interval.Element) -> Domain {
+//        return Domain()
+//    }
+
+}
+
+func +(lhs: Double, rhs: Expression) -> Expression {
+    return Expression(rhs.operations + [(lhs, +)])
+}
+
+func +(lhs: Expression, rhs: Double) -> Expression {
+    return Expression(lhs.operations + [(rhs, +)])
+}
+
+func *(lhs: Double, rhs: Expression) -> Expression {
+    return Expression(rhs.operations + [(lhs, *)])
+}
+
+func *(lhs: Expression, rhs: Double) -> Expression {
+    return Expression(lhs.operations + [(rhs, *)])
+}
+
+infix operator ^ { associativity left precedence 160 }
+func ^(lhs: Expression, rhs: Double) -> Expression {
+    return Expression(lhs.operations + [(rhs, pow)])
+}
+
+let x = Expression()
+
+let expression: Expression = 5.0*x^3.0 + 4.0
+
+expression.evaluateFor(2.0)
+
+(-100..<100).map { (i: Int) -> () in
+    expression.evaluateFor(Double(i))
+    return ()
+}
+
